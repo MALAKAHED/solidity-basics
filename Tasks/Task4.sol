9e69a71b1e753ed89acb95c630e7ed2c2b72c250
MALAKAHED <lokamalek776@gmail.com>
2026-03-12 19:31:33 +0200

    'fundInUSD'

---
+++ Tasks/Task4.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract FundMe {
+
+    uint256 minimumInUSD = 5e18;
+
+    function fund() public payable {
+        require(getConversionRate(msg.value) >= minimumInUSD, "didn't send enough ETH!");
+    }
+
+    function getPrice() public view returns(uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
+        (,int256 price,,,) = priceFeed.latestRoundData();
+        return uint256(price * 1e10);
+    }
+
+    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUSD = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUSD;
+    }
+
+    function getVersion() public view returns(uint256) {
+        return AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419).version();
+    }
+
+}
