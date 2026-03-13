599e38969645a4025ef0b6f9cd2504b28ca04440
MALAKAHED <lokamalek776@gmail.com>
2026-03-14 00:49:41 +0200

    'library'

---
+++ FundMeProject/priceConverter.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+
+library priceConverter {
+
+function getPrice() internal view returns(uint256){
+        AggregatorV3Interface pricefeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
+        (,int256 price,,,) = pricefeed.latestRoundData();
+        return uint256(price * 1e10);
+    }
+
+    function getConversionRate(uint256 ethAmount) internal view returns(uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUSD = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUSD;
+    }
+
+    function getVersion() internal view returns(uint256) {
+        return AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419).version();
+    }
+
+    }
