cff5e09ea953613716d96d92d0aeaf3e2d66c8d1
MALAKAHED <lokamalek776@gmail.com>
2026-03-14 00:49:59 +0200

    'useLibrary'

--- FundMeProject/fundMe.sol
+++ FundMeProject/fundMe.sol
@@ -1,41 +1,29 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
-
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {priceConverter} from "FundMeProject/priceConverter.sol";
 
 contract fundMe {
 
-// function fund allow paople to send mony 
-// minimum value to send
+    using priceConverter for uint256;
+
     uint public minimumUsd = 5e18;
 
     address[] public funders;
     mapping(address funder => uint256 ammountFunded) public adressToAmountFunded;
 
-
     function fund() public payable {
 
-        require(getConversionRate(msg.value) >= minimumUsd, "didn't send enough ETH!");
+        require(msg.value.getConversionRate() >= minimumUsd, "didn't send enough ETH!");
         funders.push(msg.sender);
-        adressToAmountFunded[msg.sender] = adressToAmountFunded[msg.sender] + msg.value;
+        adressToAmountFunded[msg.sender] += msg.value;
     }
-
- 
-    function getPrice() public view returns(uint256){
-        AggregatorV3Interface pricefeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
-        (,int256 price,,,) = pricefeed.latestRoundData();
-        return uint256(price * 1e10);
+    
+    function withDraw() public {
+      //for(starting index ,       ended index,                   step amount) {}
+        for(uint256 funderIndex = 0; funderIndex = funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            adressToAmountFunded[funder] = 0;
+        }
     }
-
-    function getConversionRate(uint256 ethAmount) public view returns(uint256) {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUSD = (ethPrice * ethAmount) / 1e18;
-        return ethAmountInUSD;
-    }
-
-    function getVersion() public view returns(uint256) {
-        return AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419).version();
-    }
-        // function withDraw() public {}
 } 
